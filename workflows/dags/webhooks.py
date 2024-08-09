e5a371cb405016b65cb30cdc73baa1cdca633f85
Pulkit Garg <wittycodes@gmail.com>
2024-08-10 02:05:11 +0530

    Update webhooks.py

--- workflows/dags/webhooks.py
+++ workflows/dags/webhooks.py
@@ -159,7 +159,7 @@ with DAG('node_cpu_analysis', default_args=default_args, schedule_interval=None)
         python_callable=lambda ti: [
             slack.send_to_slack(ti),
             pagerduty.send_to_pagerduty(ti),
-            customemail.send_to_email(ti),
+            customemail.send_to_custom_email(ti),
             discord.send_to_discord(ti)
         ],
         dag=dag
