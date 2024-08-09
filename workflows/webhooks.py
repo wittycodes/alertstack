e328ea04ce909fa0b37be04e87463acb267abdcf
Pulkit Garg <wittycodes@gmail.com>
2024-08-09 10:21:45 +0530

    init

--- workflows/webhooks.py
+++ workflows/webhooks.py
@@ -8,7 +8,7 @@ default_args = {
     'start_date': datetime(2021, 1, 1)
 }
 
-with DAG('example_http_operator', default_args=default_args, schedule_interval=None) as dag:
+with DAG('webhook_http_operator', default_args=default_args, schedule_interval=None) as dag:
     task = SimpleHttpOperator(
         task_id='post_op',
         http_conn_id='http_default',
