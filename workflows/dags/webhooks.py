4697221036a5919bd6c2c64ee4122fa9e9cfe3b3
Pulkit Garg <wittycodes@gmail.com>
2024-08-09 18:18:53 +0530

    Update webhooks.py

--- workflows/dags/webhooks.py
+++ workflows/dags/webhooks.py
@@ -3,9 +3,9 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.providers.http.operators.http import SimpleHttpOperator
-from ..notifiers import slack, pagerduty, email
-from ..actions import k8s_actions
-from ..feedbacks import k8s_feedbacks
+from notifiers import slack, pagerduty, email
+from actions import k8s_actions
+from feedbacks import k8s_feedbacks
 
 
 from datetime import datetime
