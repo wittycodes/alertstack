670535069e9839651fae6a100789441ba5bea57b
Pulkit Garg <wittycodes@gmail.com>
2024-08-10 02:12:38 +0530

    Update discord.py

--- workflows/notifiers/discord.py
+++ workflows/notifiers/discord.py
@@ -4,5 +4,5 @@ import requests
 def send_to_discord(content):
     webhook_url = 'https://discord.com/api/webhooks/1271545437198880798/JfNeqOIgIhLK9dssn7TzpzN9TWoA25Jx4Rce-1-jrWRi-iX6cvdwLfv7rff0mqrvbzRG'  # Replace with your webhook URL
 
-    data = {'content': content}
+    data = {'content': "hu ha content"}
     response = requests.post(webhook_url, json=data)
