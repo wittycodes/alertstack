d2e8c1bc8bc4469e74e78c0cbde4f1d9c54c92c7
Pulkit Garg <wittycodes@gmail.com>
2024-08-11 17:19:07 +0530

    Update main.py

--- airflow/workflows/dags/main.py
+++ airflow/workflows/dags/main.py
@@ -1,9 +1,7 @@
 # https://www.restack.io/docs/airflow-knowledge-apache-webhook-connect-rest-api-providers-http-pypi
 
 from airflow import DAG
-from airflow.operators.python import PythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.providers.http.operators.http import SimpleHttpOperator
 from notifiers import slack, pagerduty, customemail, discord
 from actions import k8s_actions
 from feedbacks import k8s_feedbacks
@@ -22,7 +20,7 @@ with DAG('webhook_prometheus_entrypoint', default_args=default_args, schedule_in
         task_id='pod_volume_analysis_trigger_dag',
         trigger_dag_id='pod_volume_analysis',
         # conf={'my_param': 'value'},
-        wait_for_completion=True,
+        wait_for_completion=False,
     )
 
     pod_memory_analysis_trigger_dag = TriggerDagRunOperator(
@@ -36,10 +34,10 @@ with DAG('webhook_prometheus_entrypoint', default_args=default_args, schedule_in
         task_id='node_cpu_analysis_trigger_dag',
         trigger_dag_id='node_cpu_analysis',
         # conf={'my_param': 'value'},
-        wait_for_completion=True,
+        wait_for_completion=False,
     )
 
-    pod_volume_analysis_trigger_dag >> [ node_cpu_analysis_trigger_dag, pod_volume_analysis_trigger_dag]
+    pod_volume_analysis_trigger_dag >> [ node_cpu_analysis_trigger_dag, pod_memory_analysis_trigger_dag]
 
 
 with DAG('pod_volume_analysis', default_args=default_args, schedule_interval=None) as dag:
